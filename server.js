2784bc73202f5292e1185c7e4fadd887ea5535e6
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2024-02-28 14:58:46 +0100

    Using basic routing

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
+// Create a server
 const server = http.createServer((req, res) => {
   console.log("Server received a request");
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path += "contact.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/contact");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log("Error reading file");
+      res.end();
+    }
+    res.end(data);
+  });
 });
 
 server.listen(3000, () => {
